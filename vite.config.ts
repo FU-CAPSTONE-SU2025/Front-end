46410cb7c63a01e5ca0e9ad33811a28d1e973b5c
Hendrix Blaze <blazehendrix007@gmail.com>
2025-05-25 15:45:31 +0700

    github page deploy
    Alt if no docker

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
+import { githubPagesSpa } from "@sctg/vite-plugin-github-pages-spa";
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+      githubPagesSpa({
+      // Options are optional. This is the added part
+      verbose: true, // Set to false to disable console logs
+    }),
+  ],
   base: '/Front-end/',
+  //this too is the added part
+  
 })
