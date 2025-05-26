377840b66a9d70e0e4b90410c537934abfa7a599
Hendrix Blaze <blazehendrix007@gmail.com>
2025-05-26 22:58:51 +0700

    remove github page

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,9 @@
 import { defineConfig } from 'vite'
-import { githubPagesSpa } from "@sctg/vite-plugin-github-pages-spa";
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
       react(),
-      githubPagesSpa({
-      // Options are optional. This is the added part
-      verbose: true, // Set to false to disable console logs
-    }),
   ],
-  base: '/Front-end/',
-  //this too is the added part
-  // The repo name aka github base url, maybe lol
-  
 })
