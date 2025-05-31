a395c9beec97ac8c8ae754a5ed6ad777abd74981
Hendrix Blaze <blazehendrix007@gmail.com>
2025-05-31 13:43:03 +0700

    data dummy

---
+++ data/mockAdmin.js
@@ -0,0 +1,5 @@
+admins = [
+    {"Id": "ADMN000001", "Email": "nguyen.vana.admin@gmail.com", "Name": "Nguyen Van A Admin", "Phone Number": "0901234567", "Address": "123 Le Loi St, District 1, Ho Chi Minh City", "Campus": "HCMC Campus", "Add Dated": "15/01/2024"},
+    {"Id": "ADMN000002", "Email": "tran.thib.admin@yahoo.com", "Name": "Tran Thi B Admin", "Phone Number": "0912345678", "Address": "45 Nguyen Trai St, District 5, Ho Chi Minh City", "Campus": "Ha Noi Campus", "Add Dated": "20/02/2024"},
+    {"Id": "ADMN000003", "Email": "le.vanc.admin@gmail.com", "Name": "Le Van C Admin", "Phone Number": "0933456789", "Address": "78 Hai Ba Trung St, Ha Noi", "Campus": "Da Nang Campus", "Add Dated": "10/03/2024"}
+  ]
