3b97c0b2a14224d418eabfbf44bead154371f345
Andy <vaper2212@gmail.com>
2025-06-13 23:22:37 +0700

    update bookingAdvisor 60% UI

--- src/components/student/layout.tsx
+++ src/components/student/layout.tsx
@@ -5,15 +5,15 @@ import { Outlet } from 'react-router';
 
 const StudentLayout: React.FC = () => {
   return (
-  
-     <>
-      <Header />
-     
+    <>
+      <div className="fixed top-0 left-0 w-full z-50">
+        <Header />
+      </div>
+      <div className="bg-gradient-to-br from-orange-500 to-blue-900 pt-20 min-h-screen">
         <Outlet />
-     
+      </div>
       <Footer />
-     </>
-   
+    </>
   );
 };
 
